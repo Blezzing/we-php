3f8b73de0daf4740bb3a302d29e19695b1a3b5cb
Thomas Bork <thomashbork@gmail.com>
2016-09-27 11:03:57 +0200

    Form post

--- views/home.php
+++ views/home.php
@@ -1,1 +1,15 @@
-HEJ TIL VERDEN!
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<form action="views/formposttest.php" method="post">
+Name: <input type="text" name="name"><br>
+Favorite color: <input type="text" name="color"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
