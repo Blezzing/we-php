43a7a5784319aeaeffc68ea49f3f3b0ae2638107
blezzing <mmhmaster@hotmail.com>
2016-09-27 21:03:01 +0000

    Prettified

---
+++ views/nameAndColorForm.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page Title</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="/js/functions.js"></script>
+</head>
+<body>
+    <form action="formposttest.php" method="post">
+        Name: <input type="text" name="name"><br>
+        Favorite color: <input type="text" name="color"><br>
+        <input type="submit">
+    </form>
+
+    <hr>
+
+    <input type="text" id="squareText">
+    <button id="btn-get-square" type="button">Square this!</button>
+</body>
+</html>
--- views/xmlViewer.php
+++ views/xmlViewer.php
@@ -1,26 +1,36 @@
-<?php
-require "../controllers/xmlController.php";
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page Title</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="js/functions.js"></script>
+</head>
+<body>
+	<?php
+	require "../controllers/xmlController.php";
 
-function getNationDocAsList($doc){
-    $list = "<ul>";
-    foreach ($doc->T as $nation) {
-        $list .= "<li>";
-        $list .= $nation->N_NAME;
-        $list .= "</li>";
-    }
-    $list .= "</ul>";
-    return $list;
-}
+	function getNationDocAsList($doc){
+	    $list = "<ul>";
+	    foreach ($doc->T as $nation) {
+	        $list .= "<li>";
+	        $list .= $nation->N_NAME;
+	        $list .= "</li>";
+	    }
+	    $list .= "</ul>";
+	    return $list;
+	}
 
-$doc = XMLController::getNationDoc();
+	$doc = XMLController::getNationDoc();
 
-$nationList = getNationDocAsList($doc);
+	$nationList = getNationDocAsList($doc);
 
-foreach ($doc->T as $nation) {
-    $nation->N_NAME = ucfirst(strtolower($nation->N_NAME));
-}
+	foreach ($doc->T as $nation) {
+    	$nation->N_NAME = ucfirst(strtolower($nation->N_NAME));
+	}
 
-$denmarkList = getNationDocAsList($doc);
+	$denmarkList = getNationDocAsList($doc);
 
-echo($nationList . "<hr>" . $denmarkList);
+	echo($nationList . "<hr>" . $denmarkList);
 ?>
+</body>
+</html>
