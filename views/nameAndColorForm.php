3648aaa6b8411d7bffcca52319b53501a1b22f31
Cleevil <Cleevil@users.noreply.github.com>
2016-09-28 21:43:13 +0200

    . fix!

--- views/nameAndColorForm.php
+++ views/nameAndColorForm.php
@@ -3,7 +3,7 @@
 <head>
     <title>Page Title</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="/js/functions.js"></script>
+    <script src="../js/functions.js"></script>
 </head>
 <body>
     <form action="formposttest.php" method="post">
