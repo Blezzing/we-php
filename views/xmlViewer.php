44fceb610a6f20372cc02e1835edd260323d9844
Thomas Bork <thomashbork@gmail.com>
2016-09-28 21:09:26 +0200

    Prettify

--- views/xmlViewer.php
+++ views/xmlViewer.php
@@ -28,9 +28,9 @@
     	$nation->N_NAME = ucfirst(strtolower($nation->N_NAME));
 	}
 
-	$denmarkList = getNationDocAsList($doc);
+	$prettyList = getNationDocAsList($doc);
 
-	echo($nationList . "<hr>" . $denmarkList);
+	echo($nationList . "<hr>" . $prettyList);
 ?>
 </body>
 </html>
