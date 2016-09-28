4a8d18d2b7329d6aa19e6d8d856741e15be1d009
Cleevil <Cleevil@users.noreply.github.com>
2016-09-28 18:43:18 +0200

    Get and print from XML

--- views/home.php
+++ views/home.php
@@ -11,8 +11,17 @@
         Favorite color: <input type="text" name="color"><br>
         <input type="submit">
     </form>
-    <hr>
+    
+    <?php
+        $xml = simplexml_load_file("http://www.cs.washington.edu/research/xmldatasets/data/tpc-h/nation.xml");
+        foreach ($xml->T as $land) {
+            echo $land->N_NAME."<br>";
+        }
+    ?>
     X: <input type="text" id="squareText"></br>
     <button id="btn-get-square" type="button">Get square</button>
+
+    <br><br>
+    <?php phpinfo() ?>
 </body>
 </html>
