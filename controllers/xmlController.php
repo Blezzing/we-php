3f4fcd6514794ae6e59213e790439952450ab02c
Thomas Bork <thomashbork@gmail.com>
2016-09-28 21:02:41 +0200

    Pæn kode!

--- controllers/xmlController.php
+++ controllers/xmlController.php
@@ -4,7 +4,6 @@ class XMLController {
     public static function getNationDoc(){
         return simplexml_load_file("http://www.cs.washington.edu/research/xmldatasets/data/tpc-h/nation.xml");
     }
-    
 } 
 
 ?>
